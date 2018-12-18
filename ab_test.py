d3da2d7d926cbe7a8f7a48fb126b7f423ab1e644
penpen <zhang15991335671@163.com>
2018-12-18 16:14:17 +0800

    matomo计算数据

--- ab_test.py
+++ ab_test.py
@@ -123,9 +123,9 @@ class AbTest(object):
         visit_uid = str(uuid.uuid1()).replace('-', '')
         actiondetails = dct.pop('actionDetails')
         ret['visit_details'] = dict([(k.lower(), v) for k, v in dct.items() if k.lower() in self.fields.get('visit_details')])
-        ret['visit_details'].update({'uid': visit_uid})
         cvs = dct.get('customVariables')
-        ret['user_type'] = cvs.get('customVariableValue1') if cvs else ''
+        cvs = cvs.get('customVariableValue1') if cvs else ''
+        ret['visit_details'].update({'uid': visit_uid, 'user_type': cvs})
 
         # sort_val = self.get_max_vid(ret.get('visitorid'))     # 访问者的访问事件记录递增
         for x in self.detail_struct:
